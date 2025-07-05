372b43c0b5f809a31b5e139e6aeafce8a1162d0b
HaoJenShih <65109974+HaoJenShih@users.noreply.github.com>
2025-07-05 15:54:00 +0100

    Add files via upload
    The frontend, backend code of Summer Project

---
+++ SummerProject_code/web/src/services/api.service.ts
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+// const api = axios.create({baseURL: 'https://api.hlt.prototypes.legalpythia.com'});
+const api = axios.create({baseURL: 'https://api.bma.demos.legalpythia.com'});
+// const api = axios.create({baseURL: 'http://127.0.0.1:8000'});
+
+export class APIService {
+  static async uploadFile(file: File, session_id: string) {
+    const fd = new FormData();
+    fd.append('file', file);
+    fd.append('session_id', session_id);
+
+    const response = await api.post('/v1/upload', fd, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    return response.data;
+  }
+
+  static async validateUpload(summary: string, companyName: string, session_id: string) {
+    const jsonData = JSON.stringify({
+      summary,
+      companyName,
+      session_id,
+    });
+
+    const response = await api.post('/v1/validate', jsonData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return response.data;
+  }
+
+  static async sendChatMessage(message: string, session_id: string) {
+    const response = await api.post(
+      '/v1/chat',
+      {message, session_id},
+      {
+        adapter: 'fetch',
+        headers: {'Content-Type': 'application/json'},
+        responseType: 'stream',
+      }
+    );
+
+    return response.data;
+  }
+}
